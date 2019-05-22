93d8aef446371522945cb0c1eaeec46c1dc5d8e8
MBoburjon <mukhammadboburjon@gmail.com>
2019-05-23 01:13:20 +0900

    fixing listView and DetailView bugs and edding category option

--- meals/admin.py
+++ meals/admin.py
@@ -3,7 +3,9 @@ from django.contrib import admin
 # Register your models here.
 
 
-from .models import Meals
+from .models import Meals, Category
 
 
 admin.site.register(Meals)
+admin.site.register(Category)
+

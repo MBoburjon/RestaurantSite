244f3ccdefaacbce5699eafcd78e76ac23118050
MBoburjon <mukhammadboburjon@gmail.com>
2019-05-25 06:33:23 +0900

    bug fixing and adding styles

--- reservation/views.py
+++ reservation/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from .forms import ReserveTableForm
 # Create your views here.
+
+from reservation.models import Reservation
+
+def reserve_table(request):
+
+    context = {}
+
+    return render(request, 'reservation/reservation.html', context)
